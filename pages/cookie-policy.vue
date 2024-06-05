996088b0b4a768520672831769df2751974caabd
Volodislav Penev <penev.vladislav@gmail.com>
2024-06-05 13:51:18 +0300

    Added cookies settings in cookies page

--- pages/cookie-policy.vue
+++ pages/cookie-policy.vue
@@ -1,20 +1,29 @@
 <template>
-  <main class="page cookie-page" >
+  <main class="page cookie-page">
     <section class="c-cookie">
       <div class="-w">
         <div class="title -a-p -tac -split" data-string>
-          <h1 class="-h5" >
+          <h1 class="-h5">
             <span data-string-split style="--l-delay: 0.6;">Atlas Squad Cookie Policy</span>
           </h1>
           <p class="-m -a-to-top">Effective Date: 7 May 2024</p>
         </div>
         <div class="cookie-content -a-to-top" style="--l-delay: 0.15" data-string>
-          
-          <p>At Atlas Squad, we believe in being clear and open about how we collect and use data related to you. This Cookie Policy applies to any Atlas Squad product or service that links to this policy or incorporates it by reference. We use cookies and similar technologies (collectively referred to in this policy as “cookies”) to collect and use data as part of our Services, as defined in our Privacy Policy (“Services”) and which includes our website, as defined in our Privacy Policy (“Website”), and. In the spirit of transparency, this policy provides detailed information about how and when we use these technologies.</p>
-          
+
+          <p>At Atlas Squad, we believe in being clear and open about how we collect and use data related to you. This
+            Cookie Policy applies to any Atlas Squad product or service that links to this policy or incorporates it by
+            reference. We use cookies and similar technologies (collectively referred to in this policy as “cookies”) to
+            collect and use data as part of our Services, as defined in our Privacy Policy (“Services”) and which
+            includes our website, as defined in our Privacy Policy (“Website”), and. In the spirit of transparency, this
+            policy provides detailed information about how and when we use these technologies.</p>
+
           <h2 class="-h6">Definition of cookies</h2>
-          <p>A cookie is a small piece of data (text file) that a website – when visited by a user – asks your browser to store on your device in order to remember information about you, such as your language preference or login information. Those cookies are set by us and called first party cookies. We also use third party cookies – which are cookies from a domain different than the domain of the website you are visiting – for our advertising and marketing efforts.</p>
-          
+          <p>A cookie is a small piece of data (text file) that a website – when visited by a user – asks your browser
+            to store on your device in order to remember information about you, such as your language preference or
+            login information. Those cookies are set by us and called first party cookies. We also use third party
+            cookies – which are cookies from a domain different than the domain of the website you are visiting – for
+            our advertising and marketing efforts.</p>
+
           <h2 class="-h6">Why we are using cookies</h2>
           <p>More specifically, we use cookies and other tracking technologies for the following purposes:</p>
           <ul>
@@ -25,7 +34,9 @@
           </ul>
 
           <h2 class="-h6">What type of cookies we are using</h2>
-          <p>Below is a detailed list of the cookies we use on our Website. Our Website is scanned with our cookie scanning tool regularly to maintain a list as accurate as possible. We classify cookies in the following categories:</p>
+          <p>Below is a detailed list of the cookies we use on our Website. Our Website is scanned with our cookie
+            scanning tool regularly to maintain a list as accurate as possible. We classify cookies in the following
+            categories:</p>
           <ul>
             <li>Strictly Necessary Cookies</li>
             <li>Performance Cookies</li>
@@ -34,18 +45,26 @@
           </ul>
 
           <h2 class="-h6">OPT-OUT</h2>
-          <p>You can opt-out of each cookie category (except strictly necessary cookies) by clicking on the “cookie settings” button below:</p>
-          <NuxtLink to="/" class="-link -up -b">Customize Settings</NuxtLink>
+          <p>You can opt-out of each cookie category (except strictly necessary cookies) by clicking on the “cookie
+            settings” button below:</p>
+          <NuxtLink to="" @click="cookiesSettings" class="-link -up -b">Customize Settings</NuxtLink>
 
           <h2 class="-h6">What is Do Not Track (DNT)?</h2>
-          <p>DNT is a concept that has been promoted by regulatory agencies such as the U.S. Federal Trade Commission (FTC), for the Internet industry to develop and implement a mechanism for allowing Internet users to control the tracking of their online activities across websites by using browser settings. As such, LinkedIn does not generally respond to “do not track” signals.</p>
+          <p>DNT is a concept that has been promoted by regulatory agencies such as the U.S. Federal Trade Commission
+            (FTC), for the Internet industry to develop and implement a mechanism for allowing Internet users to control
+            the tracking of their online activities across websites by using browser settings. As such, LinkedIn does
+            not generally respond to “do not track” signals.</p>
 
           <h2 class="-h6">Cookie List</h2>
           <p class="-b">We use cookies and other tracking technologies for the following purposes:</p>
 
           <h3 class="-up -p -b">Strictly Necessary Cookies</h3>
-          <p>These cookies are necessary for the website to function and cannot be switched off in our systems. They are usually only set in response to actions made by you which amount to a request for services, such as setting your privacy preferences, logging in or filling in forms. You can set your browser to block or alert you about these cookies, but some parts of the site will not then work. These cookies do not store any personally identifiable information.</p>
-          
+          <p>These cookies are necessary for the website to function and cannot be switched off in our systems. They are
+            usually only set in response to actions made by you which amount to a request for services, such as setting
+            your privacy preferences, logging in or filling in forms. You can set your browser to block or alert you
+            about these cookies, but some parts of the site will not then work. These cookies do not store any
+            personally identifiable information.</p>
+
           <div class="-table">
             <div class="-thead -p" style="--tw: 1;">
               <div class="-tcolumn" style="--tr: 1;">
@@ -74,8 +93,11 @@
 
 
           <h3 class="-up -p -b">Analytics Cookies</h3>
-          <p>These cookies allow us to count visits and traffic sources, so we can measure and improve the performance of our site. They help us know which pages are the most and least popular and see how visitors move around the site. All information these cookies collect is aggregated and therefore anonymous. If you do not allow these cookies, we will not know when you have visited our site.</p>
-          
+          <p>These cookies allow us to count visits and traffic sources, so we can measure and improve the performance
+            of our site. They help us know which pages are the most and least popular and see how visitors move around
+            the site. All information these cookies collect is aggregated and therefore anonymous. If you do not allow
+            these cookies, we will not know when you have visited our site.</p>
+
           <div class="-table">
             <div class="-thead -p" style="--tw: 1;">
               <div class="-tcolumn" style="--tr: 1;">
@@ -98,8 +120,10 @@
 
 
           <h3 class="-up -p -b">Functional Cookies</h3>
-          <p>These cookies enable the website to provide enhanced functionality and personalization. They may be set by us or by third party providers whose services we have added to our pages.    If you do not allow these cookies then some or all of these services may not function properly.</p>
-          
+          <p>These cookies enable the website to provide enhanced functionality and personalization. They may be set by
+            us or by third party providers whose services we have added to our pages.    If you do not allow these
+            cookies then some or all of these services may not function properly.</p>
+
           <div class="-table">
             <div class="-thead -p" style="--tw: 1;">
               <div class="-tcolumn" style="--tr: 1;">
@@ -122,8 +146,11 @@
 
 
           <h3 class="-up -p -b">Targeting Cookies</h3>
-          <p>Targeting cookies may be set through our site by our advertising partners. They can be used by these third parties to build a profile of your interests based on the browsing information they collect from you, which includes uniquely identifying your browser and terminal equipment. If you do not allow these cookies you will still see basic advertising on your browser that is generic and not based on your interests.</p>
-          
+          <p>Targeting cookies may be set through our site by our advertising partners. They can be used by these third
+            parties to build a profile of your interests based on the browsing information they collect from you, which
+            includes uniquely identifying your browser and terminal equipment. If you do not allow these cookies you
+            will still see basic advertising on your browser that is generic and not based on your interests.</p>
+
           <div class="-table">
             <div class="-thead -p" style="--tw: 1;">
               <div class="-tcolumn" style="--tr: 1;">
@@ -150,7 +177,7 @@
             </div>
           </div>
 
-          
+
         </div>
       </div>
     </section>
@@ -160,12 +187,18 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import GlobalClass from '@/src/globalClass'
+import StringCookies from '~/src/string-cookies';
 
 const nuxtApp = useNuxtApp()
 const global = nuxtApp.$globalClass as GlobalClass
 
 
 const checkedTerms = ref(false);
+
+const cookiesSettings = () => {
+  StringCookies.getInstance().showSettings({ title: 'Title', description: 'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full ', email: `penev.vladislav@gmail.com` });
+
+}
 
 onMounted(() => {
 
@@ -182,7 +215,7 @@ onBeforeUnmount(() => {
 .page {
   .c-cookie {
     position: relative;
-    
+
     .-w {
       .title {
         margin-bottom: 2rem;
@@ -191,8 +224,9 @@ onBeforeUnmount(() => {
           color: var(--c-grey-0);
         }
       }
+
       .cookie-content {
-        
+
 
         h2,
         h3 {
@@ -200,14 +234,17 @@ onBeforeUnmount(() => {
           margin-top: 3rem;
           margin-bottom: 1.5rem;
         }
+
         p {
           margin-top: 1rem;
           margin-bottom: 1rem;
         }
+
         ul {
           margin-top: 2rem;
           margin-bottom: 2rem;
         }
+
         .-table {
           margin-top: 2rem;
           margin-bottom: 2rem;
@@ -230,6 +267,4 @@ onBeforeUnmount(() => {
     }
   }
 }
-
-
 </style>
